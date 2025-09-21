1b75672275f3b628252a8c50c4f556d8c2040eec
Haru-Tachibana <paloma030415@gmail.com>
2025-09-21 13:48:42 +0100

    Add PWA support and Netlify configuration

---
+++ web/sw.js
@@ -0,0 +1,44 @@
+// Service Worker for Kanso PWA
+const CACHE_NAME = 'kanso-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/main.dart.js',
+  '/flutter.js',
+  '/manifest.json',
+  '/icons/Icon-192.png',
+  '/icons/Icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // Return cached version or fetch from network
+        return response || fetch(event.request);
+      })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
